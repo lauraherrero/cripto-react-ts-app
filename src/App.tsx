e55b88418138695bbc3dc73a394f9d01633c10e5
Laura Herrero Poza <lherrero@nextdigital.es>
2024-06-06 13:48:06 +0200

    Add styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ function App() {
 
   return (
     <>
-    <div className="container"></div>
-      <h1 className="app-title">Cotizador de <span>Criptomonedas</span></h1>
-      <div className="content"></div>
+      <div className="container">
+        <h1 className="app-title">Cotizador de <span>Criptomonedas</span></h1>
+        <div className="content"></div>
+      </div>
+
     </>
   )
 }
