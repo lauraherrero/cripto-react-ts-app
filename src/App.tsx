7045e87dff127bd92df14a33575d77ef0072a048
Laura Herrero Poza <lherrero@nextdigital.es>
2024-06-06 13:38:31 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+    <div className="container"></div>
+      <h1 className="app-title">Cotizador de <span>Criptomonedas</span></h1>
+      <div className="content"></div>
+    </>
+  )
+}
+
+export default App
