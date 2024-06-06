3fab4a328f506e3d43f69b3d772c091c0ce0c000
Laura Herrero Poza <lherrero@nextdigital.es>
2024-06-06 19:16:18 +0200

    Get the cryptocurrencies and print with the console

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,20 @@
 import { create } from 'zustand';
 
+export const getCryptos = async() => {
+  const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
+
+  const resp = await fetch( url );
+  const data = await resp.json();
+  const result = await data.Data
+
+  console.log(result);
+  
+}
+
 export const useCryptoStore = create(() => ({
   fetchCryptos: () => {
     console.log('Desde fecth');
-    
+    getCryptos();
+   
   }
 }))
